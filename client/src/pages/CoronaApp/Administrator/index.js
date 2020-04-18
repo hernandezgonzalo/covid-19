e9a542359f6656613d2b283ff168430ff7f2cebd
Gonzalo Hernández <gonzalohernandez@icloud.com>
2020-04-19 01:31:45 +0200

    administrator control panel queries

--- client/src/pages/CoronaApp/Administrator/index.js
+++ client/src/pages/CoronaApp/Administrator/index.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { createRef } from "react";
 import { Grid, Avatar } from "@material-ui/core";
 import MaterialTable from "material-table";
-import { searchUsers } from "../../../services/adminService";
+import { searchUsers, findCase } from "../../../services/adminService";
 import { useStyles } from "./styles";
 import TimeAgo from "../../../components/ui/TimeAgo";
+import { useHistory } from "react-router-dom";
 
 const Administrator = () => {
+  const tableRef = createRef();
   const classes = useStyles();
+  const history = useHistory();
 
   const handleUpdateData = async query => {
-    console.log(query);
-    const { pageSize, page, search } = query;
+    const { pageSize, page, search, orderBy, orderDirection } = query;
     try {
-      const res = await searchUsers(pageSize, page, search);
+      const res = await searchUsers(
+        pageSize,
+        page,
+        search,
+        orderBy,
+        orderDirection
+      );
       return res;
     } catch (error) {
       console.log(error.message);
@@ -26,13 +34,21 @@ const Administrator = () => {
       pageSize: 20,
       pageSizeOptions: [],
       showTitle: false,
-      actionsColumnIndex: 6
+      actionsColumnIndex: 6,
+      draggable: false,
+      addRowPosition: "first"
     }
+  };
+
+  const handleOnRowClick = async (e, selectedRow) => {
+    const { caseToShow } = await findCase(selectedRow.case);
+    history.push("/app", { caseToShow });
   };
 
   return (
     <Grid item lg={6} className={classes.tableWrapper}>
       <MaterialTable
+        tableRef={tableRef}
         {...options}
         columns={[
           {
@@ -45,15 +61,17 @@ const Administrator = () => {
                 src={rowData.image}
                 className={classes.image}
               />
-            )
+            ),
+            editComponent: () => <Avatar className={classes.image} />
           },
-          { title: "Name", field: "name" },
-          { title: "Surname", field: "surname" },
-          { title: "City", field: "city" },
-          { title: "Country", field: "country" },
+          { title: "Name", field: "name", customSort: a => a },
+          { title: "Surname", field: "surname", customSort: a => a },
+          { title: "City", field: "city", customSort: a => a },
+          { title: "Country", field: "country", customSort: a => a },
           {
             title: "Date",
             field: "date",
+            defaultSort: "desc",
             render: rowData => <TimeAgo date={new Date(rowData.date)} />
           }
         ]}
@@ -61,7 +79,7 @@ const Administrator = () => {
           new Promise((resolve, reject) => {
             handleUpdateData(query).then(res =>
               resolve({
-                data: res.cases,
+                data: res.users,
                 page: res.page,
                 totalCount: res.count
               })
@@ -69,11 +87,31 @@ const Administrator = () => {
           })
         }
         editable={{
+          onRowAdd: newData =>
+            new Promise(resolve => {
+              console.log(newData);
+              resolve();
+            }),
+          onRowUpdate: (newData, oldData) =>
+            new Promise((resolve, reject) => {
+              console.log(newData, oldData);
+              resolve();
+            }),
           onRowDelete: oldData =>
             new Promise(resolve => {
+              console.log(oldData);
               resolve();
             })
         }}
+        actions={[
+          {
+            icon: "refresh",
+            tooltip: "Refresh data",
+            isFreeAction: true,
+            onClick: () => tableRef.current && tableRef.current.onQueryChange()
+          }
+        ]}
+        onRowClick={handleOnRowClick}
       />
     </Grid>
   );
--- client/src/services/adminService.js
+++ client/src/services/adminService.js
@@ -5,7 +5,28 @@ const api = axios.create({
   withCredentials: true
 });
 
-export const searchUsers = async (pageSize, page, search) => {
-  const res = await api.post("/", { pageSize, page, search });
+export const searchUsers = async (
+  pageSize,
+  page,
+  search,
+  orderBy,
+  orderDirection
+) => {
+  const res = await api.post("/", {
+    pageSize,
+    page,
+    search,
+    orderBy,
+    orderDirection
+  });
   return res.data;
+};
+
+export const findCase = async caseId => {
+  try {
+    const res = await api.post("/find-case", { caseId });
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
 };
