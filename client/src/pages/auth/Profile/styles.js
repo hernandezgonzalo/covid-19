b57fcddd588370063a54fe51011cb5c3cf456f9f
Gonzalo Hernández <gonzalohernandez@icloud.com>
2020-05-15 15:23:42 +0200

    update profile

---
+++ client/src/pages/auth/Profile/styles.js
@@ -0,0 +1,26 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.text.icon,
+    width: 100,
+    height: 100
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  },
+  input: {
+    display: "none"
+  }
+}));
