c9aaabd61e2b6716906f64a66c11e154fe573e63
Gonzalo Hernández <gonzalohernandez@icloud.com>
2020-05-26 20:17:16 +0200

    fix: undo last changes on news API

--- client/src/services/news.api.js
+++ client/src/services/news.api.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-axios.defaults.headers.post["Content-Type"] =
-  "application/x-www-form-urlencoded";
-
 const api = axios.create({
   baseURL: process.env.REACT_APP_NEWS_API_URL,
   headers: { Authorization: process.env.REACT_APP_NEWS_API_KEY }
